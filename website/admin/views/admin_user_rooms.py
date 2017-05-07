d5efcf1218005908bc7d885cf968a0eef958cd7b
David <dz15376@my.bristol.ac.uk>
2017-05-07 16:48:34 +0100

    fixed faulty and admin homepage for video

--- website/admin/views/admin_user_rooms.py
+++ website/admin/views/admin_user_rooms.py
@@ -10,8 +10,29 @@ def user(user_id):
     rooms = data_interface.get_rooms_for_user(user_id)
     user_info = data_interface.get_user_info(user_id)
     form = AddNewRoomForm()
+    devices = []
+    len_rooms = [len(rooms)]
+    room_no = [0]
+    for room in rooms:
+        devices.append(data_interface.get_room_devices(room['room_id']))
+    for room in devices:
+        for device in room:
+            if devices[0][0]['status']['last_temperature'] < 5:
+                temp_color = "black"
+            elif devices[0][0]['status']['last_temperature'] < 10:
+                temp_color = "#1d40c1"
+            elif devices[0][0]['status']['last_temperature'] < 15:
+                temp_color = '#04abd1'
+            elif devices[0][0]['status']['last_temperature'] < 20:  # not too visible against white b/g
+                temp_color = '#0ddb66'
+            elif devices[0][0]['status']['last_temperature'] < 25:
+                temp_color = '#f6f918'
+            elif devices[0][0]['status']['last_temperature'] < 30:
+                temp_color = '#f95717'
+            else:
+                temp_color = '#ff0000'
     return render_template("admin/admin_user_home.html", new_room_form=form, admin=True, rooms=rooms, user=user_info,
-                           user_name=user_info["name"])
+                           user_name=user_info["name"], room_no=room_no, len_rooms=len_rooms, devices=devices, temp_color=temp_color)
 
 
 @admin_site.route('/user/<string:user_id>/room/add', methods=['POST', 'GET'])
